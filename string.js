86dc5dc67542a9c7b59c4e02adbfab1c0f07d88e
hcxowe <hcxowe@126.com>
2019-05-16 09:56:00 +0800

    repeat

--- string.js
+++ string.js
@@ -38,7 +38,7 @@ function repeat(target, n) {
     } */
 
     // 方法四
-    var s = target, total = []
+    /* var s = target, total = []
     while (n > 0) {
         if (n % 2 == 1) {
             total[total.length] = s
@@ -52,7 +52,49 @@ function repeat(target, n) {
         n = n >> 1
     }
 
-    return total.join('')
+    return total.join('') */
 
+    // 方法五
+    /* var s = target, c = s.length * n
+
+    do {
+        s += s
+    }
+    while (n = n >> 1)
+
+    return s.substring(0, c) */
+
+    // 方法六
+    /* var s = target, total = ''
+    while (n > 0) {
+        if (n % 2 == 1) {
+            total += s
+        }
+
+        if (n == 1) {
+            break
+        }
+
+        s += s
+        n = n >> 1
+    }
+
+    return total */
+
+    // 方法七
+    if (n == 1) {
+        return target
+    }
+
+    var s = repeat(target, Math.floor(n / 2))
+    s += s
+    if (n % 2) {
+        s += target
+    }
+
+    return s
+
+    // 方法八
+    /* return (n <= 0) ? '' : target.concat(repeat(target, --n)) */
 }
 
