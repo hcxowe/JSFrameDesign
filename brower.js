b7a1c2aab326b76f210790c5415608a89e4132ba
hcxowe <hcxowe@126.com>
2019-05-20 15:39:30 +0800

    浏览器特征判定

---
+++ brower.js
@@ -0,0 +1,26 @@
+ie = !!document.recale
+ie = !!window.VBArray
+ie = !!window.ActiveXObject
+ie = !!window.createPopup
+ie = /*@cc_on!@*/!1
+ie = document.expando // document.all 在早期 opera firefox 版本也存在
+
+ie678 = !+"\v1"
+ie678 = !-[1, ]
+ie678 = '\v' == 'v'
+
+ie8 = !!window.XdomainRequest
+ie9 = document.documentMode && document.documentMode === 9
+ie10 = window.navigator.msPointerEnabled
+ie11 = '-ms-scroll-limit' in document.documentElement.style
+opera = window.opera
+firefox = window.GeckoActiveXObject
+safari = !!(navigator.verder && navigator.verder.match(/Apple/))
+chrome = !!(window.chrome && window.google)
+
+isIPhone = /iPhone/i.test(navigator.userAgent)
+isIPhone4 = window.devicePixelRatio >= 2
+isIPad = /iPad/i.test(navigator.userAgent)
+isAndroid = /android/i.test(navigator.userAgent)
+isIOS = isIPhone || isIPad
+
